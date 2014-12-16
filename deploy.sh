06e25661b9d974635b39ed755c9c2b0a16c91bf6
vincent <vincent@iZ25bflhlufZ.(none)>
2014-12-17 02:54:53 +0800

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+
 server_root=$NODE_HOME
 
 function print_log(){
-  echo "******$1******"
+  echo "$1"
 }
 
-function error_exit(){
+function errot_exit(){
   print_log "$1"
   exit 1
 }
 
+print_log "Start Deploy"
 cp -r "./" "$server_root"
+print_log "Deploy Sucessful."
